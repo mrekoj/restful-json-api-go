fd01908c0c11cae8fa7960a4980048157ef54128
Hoang Nguyen <hoangnn@gmail.com>
2018-09-20 10:19:58 +0700

    Add json struct

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"time"
 )
+
+type Todo struct {
+	Name      string    `json:"name"`
+	Completed bool      `json:"completed"`
+	Due       time.Time `json:"due"`
+}
+
+type Todos []Todo
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
@@ -23,7 +33,12 @@ func TodoShow(writer http.ResponseWriter, request *http.Request) {
 }
 
 func TodoIndex(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Todo Index!")
+	todos := Todos{
+		Todo{Name: "Write presentation"},
+		Todo{Name: "Host meetup"},
+	}
+
+	json.NewEncoder(writer).Encode(todos)
 }
 
 func Index(writer http.ResponseWriter, request *http.Request) {
